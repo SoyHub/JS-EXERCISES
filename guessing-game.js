afbe0807d83bd3e0d51be36cedeffd9c17e78320
SoyHub <sohaybmohammed@gmail.com>
2020-02-04 10:12:47 +0100

    add new features

--- guessing-game.js
+++ guessing-game.js
@@ -1,65 +1,47 @@
-// let y; 
-// alert("WELCOME TO THE GUESSING GAME!")
-
-// while ( (y !=="STOP") && (y !=="QUIT") && (y !=="EXIT") )
-// {
-//     let x= Math.floor((Math.random()*10)+1);
-//     console.log(x);
-//     y = prompt("Guess a number from 1 to 10").toUpperCase();
-    
-        
-//         if ((y == "STOP") || (y == "QUIT") || (y == "EXIT") ){
-
-//         alert("The game is shutting down!");
-//         }  else if(isNaN(y)){
-        
-//             alert("Invalid input! Try again!")
-        
-//         }  else if (y==x){
-            
-//             alert ("Nice shot!");
-//         }
-//         else if (y==x+1 ||y==x-1 ){
-            
-//             alert ("You were close! The answer is "+x);
-//         }
-        
-//         else {
-        
-//             alert("Wrong! The answer is "+ x );
-        
-//         }
-
-// }
 let num;
 function myFunction(){
     num= Math.floor((Math.random()*10)+1);
     document.getElementById('ans').innerHTML=(num);
-    let input=document.getElementById('inp').value;
+    let input=Number(document.getElementById('inp').value);
   
     if(input==num) {
-        document.getElementById('title').innerHTML=("YOU WON!");
+        document.getElementById('title').innerHTML=("NICE SHOT!");
         document.getElementById('ans').style.backgroundColor = "rgba(145, 231, 124, 0.521)";
     
-    // } else if (typeof(input)!='number') {
-        // document.getElementById('title').innerHTML=("YOU HAVE TO PUT A NUMBER!");
-        // document.getElementById('inp').style.backgroundColor="rgba(231, 124, 124, 0.521)" ;
+
     } else if (input==""){
         document.getElementById('title').innerHTML=("You have to enter something!");
         document.getElementById('ans').innerHTML=("?");
         
-    }
+    } else if (input==num+1 ||input==num-1 ){
+        document.getElementById('title').innerHTML=("THAT WAS TOO CLOSE!");
+        document.getElementById('ans').style.backgroundColor="rgba(231, 124, 124, 0.521)" ;
+
+        }
+        else if (input>10 ||input<0 ){
+            document.getElementById('title').innerHTML=("ONLY FROM 1 TO 10");
+            document.getElementById('ans').style.backgroundColor="rgba(231, 124, 124, 0.521)" ;
+            document.getElementById('ans').innerHTML=("?");
+            document.getElementById('ans').style.backgroundColor="white" ;
+            document.getElementById('inp').style.backgroundColor="rgba(231, 124, 124, 0.521)" ;
+        }
+        else if (isNaN(input)){
+            document.getElementById('title').innerHTML=("ONLY NUMBERS!");
+            document.getElementById('ans').style.backgroundColor="rgba(231, 124, 124, 0.521)" ;
+            document.getElementById('ans').innerHTML=("?");
+            document.getElementById('ans').style.backgroundColor="white" ;
+            document.getElementById('inp').style.backgroundColor="rgba(231, 124, 124, 0.521)" ;
+        }   
     
     else {
         document.getElementById('title').innerHTML=("YOU LOST!");
         document.getElementById('ans').style.backgroundColor="rgba(231, 124, 124, 0.521)" ;
     }
-    // document.getElementById('inp').value="";
-    // document.getElementById('btn').innerHTML=("TRY AGAIN!");
+ 
     document.getElementById('ans').style.color="black";
-    // document.getElementById('inp').value="";
     document.getElementById('btn').style.display="none";
     document.getElementById('btn2').style.display="block";
+    document.getElementById('inp').setAttribute('readonly', true); 
     
 }
 function myFunction2(){
@@ -69,6 +51,9 @@ function myFunction2(){
     document.getElementById('ans').innerHTML=("?");
     document.getElementById('ans').style.color="rgba(0, 0, 0, 0.233)";
     document.getElementById('ans').style.backgroundColor="white" ;
+    document.getElementById('inp').style.backgroundColor="white" ;
     document.getElementById('title').innerHTML=("Guess again from 1 to 10 !");
+    document.getElementById('inp').removeAttribute('readonly');
+
     
 }
