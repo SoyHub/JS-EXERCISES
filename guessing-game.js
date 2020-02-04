5c1b5b27bd87241a362eb9f7c18191675dd14648
SoyHub <sohaybmohammed@gmail.com>
2020-02-04 09:28:15 +0100

    fix bugs

--- guessing-game.js
+++ guessing-game.js
@@ -40,8 +40,14 @@ function myFunction(){
     if(input==num) {
         document.getElementById('title').innerHTML=("YOU WON!");
         document.getElementById('ans').style.backgroundColor = "rgba(145, 231, 124, 0.521)";
-    } else if (typeof(input)!='number') {
-        document.getElementById('title').innerHTML=("YOU HAVE TO PUT A NUMBER!");
+    
+    // } else if (typeof(input)!='number') {
+        // document.getElementById('title').innerHTML=("YOU HAVE TO PUT A NUMBER!");
+        // document.getElementById('inp').style.backgroundColor="rgba(231, 124, 124, 0.521)" ;
+    } else if (input==""){
+        document.getElementById('title').innerHTML=("You have to enter something!");
+        document.getElementById('ans').innerHTML=("?");
+        
     }
     
     else {
@@ -49,7 +55,20 @@ function myFunction(){
         document.getElementById('ans').style.backgroundColor="rgba(231, 124, 124, 0.521)" ;
     }
     // document.getElementById('inp').value="";
-    document.getElementById('btn').innerHTML=("TRY AGAIN!");
+    // document.getElementById('btn').innerHTML=("TRY AGAIN!");
     document.getElementById('ans').style.color="black";
+    // document.getElementById('inp').value="";
+    document.getElementById('btn').style.display="none";
+    document.getElementById('btn2').style.display="block";
+    
+}
+function myFunction2(){
+    document.getElementById('btn').style.display="block";
+    document.getElementById('btn2').style.display="none";
+    document.getElementById('inp').value="";
+    document.getElementById('ans').innerHTML=("?");
+    document.getElementById('ans').style.color="rgba(0, 0, 0, 0.233)";
+    document.getElementById('ans').style.backgroundColor="white" ;
+    document.getElementById('title').innerHTML=("Guess again from 1 to 10 !");
     
 }
