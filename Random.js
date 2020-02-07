b5a327c616db0f155fe91b79701f83932a4bb05a
SoyHub <sohaybmohammed@gmail.com>
2020-02-07 12:13:58 +0100

    more ex

--- Random.js
+++ Random.js
@@ -1,3 +1,4 @@
+//// Concats ex
 // // Details into variables
 // const firstName = 'Sohayb';
 // const lastName = 'Hassan';
@@ -71,6 +72,7 @@
 // console.log(cv);
 
 // //*************************************
+////fibonacci series
 
 // // var s=1000;
 // // var x= setInterval(myTimer, s);
@@ -89,6 +91,8 @@
 // // }
 
 // //*************************************
+////fibonacci series
+
 // var s2 = 1000;
 // var x2= setInterval(myTimer2, s2);
 
@@ -124,12 +128,10 @@
 //             {
 //             clearInterval(x2);
 //             }
-
-
-
 // }
 
 // //*************************************
+// print all the alphabetic numbers
 // let str="";
 // let str2="";
 
@@ -144,52 +146,224 @@
 // console.log(str);
 // console.log(str2);
 
+// another way to do it
 // let str="";
 // let x=97;
 // let y=65;
 // for (let index = 0; index < 26; index++) {
 
-//     str = str + String.fromCharCode(y);
-//     str = str + String.fromCharCode(x)+ " ";
+//     str += String.fromCharCode(y);
+//     str += String.fromCharCode(x)+ " ";
 //     x++;
 //     y++;
 
 // }
-
 // console.log(str);
 
+// Count digits in a number
 // let num = 154545;
-
 // let S= String(num);
 // let SN = S.length;
 // console.log(SN);
 
 //multi table
-let x1 = 0;
-// let y1 = 1;
-let z1 = 0;
-let str1 = "";
-let str2 = "";
+// let x1 = 0;
+// let z1 = 0;
+// let str1 = "";
+// let str2 = "";
 
-for (let index = 0; index < 10; index++) {
+// for (let index = 0; index < 10; index++) {
 
-    x1++;
-    let y1=1;
-    
-    for (let i = 0; i < 10; i++) {
+//     x1++;
+//     let y1=1;
 
-        z1 = x1 * y1;
-        str1 = x1 + "x" + y1 + "=" + z1;
-        str2 += str1 + " ";
+//     for (let i = 0; i < 10; i++) {
 
-        // if (y1 == 10) {
-        //     y1 = 0;
-        //     str2 += "\n";
-        // }
+//         z1 = x1 * y1;
+//         str1 = x1 + "x" + y1 + "=" + z1;
+//         str2 += str1 + " ";
+//         ++y1;
 
-        ++y1;
+//     }
+//     str2 += "\n";
+// }
+// console.log(str2);
 
-    }
-    str2 += "\n";
-}
-console.log(str2);
+// Get the first and the last digits...
+// let num= 45616;
+// let stNum= String(num);
+// let fDigit= "The first digit is: " + stNum[0]
+// + "\nThe second digit is: " + stNum[stNum.length-1] ;
+// console.log(fDigit);
+
+////Loops ex
+// let i = 0;
+// while(++i<5){
+//     console.log(i);
+// }
+// i=0;
+// console.log("SEPARATE")
+// while(i++<5){
+//     console.log(i);
+// }
+
+//another ex
+// let x=0;
+// for (let index = 0; index < 11; index++) {
+//     if (x % 2 == 0) {
+//         console.log(x);
+//     }
+//     x++
+// }
+// for (let index = 0; index < 11; index=index +2) {
+//    console.log(index);
+// }
+
+
+//greater than 100 ex
+// let x = prompt("Enter a number greater that 100 please!");
+// while (x<100 && x==""){
+//     x = prompt("Enter a number greater that 100 please!");
+// }
+
+//Another way
+// let x;
+// for (let index = 0; index < Infinity; index++) {
+//     if (x>100 || x =="") {
+//         break;
+//     }  
+// }
+
+//The one line solution
+//for (let x=0; isNaN(x) || x<100 && x!==null; x = prompt("Enter a number greater that 100 please!")){}
+
+
+//The solution with functions
+// let str;
+// function row (){
+//     str="";
+//     for (let i = 0; i < 7; i++) {
+//        str += "*";
+//     }
+// }
+// let str2="";
+// for (let i = 0; i < 3; i++) {
+//     row();
+//     str2 += str +"\n";
+// }
+
+// prime numbers
+// let x=0;
+// for (let i = 0; i <20;  i++) {
+// if (x%2==1) {
+//     console.log(x);
+// }
+// x++;
+
+// }
+
+
+// let num = 6316556645465566.2;
+
+
+// function decimalDetector(num){
+// if(num%1 > 0 && num%1<1 ){
+//     console.log(num+' is Decimal');
+// } else {
+//     console.log(num+" is whole");
+// }
+// }
+
+// decimalDetector(num);
+
+//Prime detector
+// let min = 50;
+// let max = 90;
+// function primeDetector(num) {
+//     if (num == 1) {
+//         return false;
+//     } else if (num == 2 || num == 3 || num == 7) {
+//         return true;
+//     } else if ((num / 2 % 1 > 0 && num / 2 % 1 < 1) && (num / 3 % 1 > 0 && num / 3 % 1 < 1) && (num / 5 % 1 > 0 && num / 5 % 1 < 1) && (num / 7 % 1 > 0 && num / 7 % 1 < 1)) {
+//         return true;
+//     }
+
+// }
+
+// for (let i = min; i < max; i++) {
+
+//     if (primeDetector(i)) {
+//         console.log(i);
+//     }
+// }
+
+
+// function DecimalDetector2(num){
+// let fnum = Math.floor(num);
+// let cnum = Math.ceil(num);
+
+// if (fnum == cnum) {
+//     console.log("The number "+num+" is whole");
+// } else {
+//     console.log("The number "+num+" is decimal");
+// }
+// }
+
+// DecimalDetector2(66.12356486);
+// decimalDetector(655);
+
+// let i = 1
+
+// while (i < 10) {
+//     if (i > 4) { break; }
+//     console.log(i)
+//     i++
+
+// }
+
+
+// function fibonacci(n) {
+//     let first = 0;
+//     let second = 1;
+//     let fib = 1;
+//     while (first + second <= n) {
+//       fib = first + second;
+//       first = second;
+//       second = fib;
+//     }
+//     console.log(fib);
+//     return fib
+//   }
+
+
+
+// function timer (t){
+//     setInterval(() => {
+//         console.log (fib);
+//     }, t);
+
+// }
+
+// for (let i = 0; i < 30; i++) {
+
+//   fibonacci(i);
+
+// }
+
+// let num =232.343466667886;
+// let str= String(num);
+
+// console.log(str.split('.')[1])
+
+
+// let str;
+// let str2 = "";
+// for (let j = 1; j < 11; j++) {
+//   str = "Table of [" + j + "]\t|";
+//   let result = ""
+//     for (let i = 1; i < 11; i++) {
+//       result = `${i}x${j} = ${i * j}\t| `
+//       str += `${result}`
+//     }
+//   console.log(str);
+// } 
